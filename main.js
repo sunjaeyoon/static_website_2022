b6824e68e8950f40f9cb961c0e4e0a3ec332e4d6
Tim <39254058+sunjaeyoon@users.noreply.github.com>
2021-12-27 12:12:56 -0500

    okay, looks cool

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
 import './style.css';
-import gsap from "https://cdn.skypack.dev/gsap";
 import * as THREE from 'three';
 import { GUI } from 'dat.gui'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import Stats from 'three/examples/jsm/libs/stats.module';
 
-/*
 //Animation Object
 var pointLight, ambientLight;
 var lightHelper, gridHelper;
 var cube;
 var plane;
+const world = {
+  plane: {
+    width: 100,
+    height: 100,
+    widthSegments: 50,
+    heightSegments: 50
+  }
+}
 
 // Make Object Function
 function createCube() {
@@ -29,8 +36,8 @@ function createAmbientLight() {
   pointLight = new THREE.PointLight(0xffffff);
   pointLight.position.set(0, 0, 10);
 
-  //ambientLight = new THREE.AmbientLight(0xffffff);
-  scene.add(pointLight)//, ambientLight);
+  ambientLight = new THREE.AmbientLight(0x543534);
+  scene.add(  pointLight)//,ambientLight,
 
   lightHelper = new THREE.PointLightHelper(pointLight)
   gridHelper = new THREE.GridHelper(200, 50);
@@ -38,7 +45,7 @@ function createAmbientLight() {
 }
 
 function createPlane() {
-  const planeGeometry = new THREE.PlaneGeometry(10, 10, 10, 10);
+  const planeGeometry = new THREE.PlaneGeometry(50, 50, 50, 50);
   const planeMaterial = new THREE.MeshPhongMaterial({
       color:0xff0000,
       side: THREE.DoubleSide,  
@@ -46,21 +53,74 @@ function createPlane() {
   });
 
   plane = new THREE.Mesh( planeGeometry, planeMaterial );
-  console.log(plane.geometry.attributes.position.array)
+  //console.log(plane.geometry.attributes.position.array)
   const array = plane.geometry.attributes.position.array;
   for(var i = 0; i < array.length; i+=3) {
     //array[i] += Math.random();
-    array[i+1] += Math.random();
-    array[i+2] += Math.random();
+    //array[i+1] += Math.random();
+    array[i+2] += Math.sin(i)//Math.random();
   }
   scene.add(plane);
 }
 
+function generatePlane() {
+  plane.geometry.dispose()
+  plane.geometry = new THREE.PlaneGeometry(
+    world.plane.width,
+    world.plane.height,
+    world.plane.widthSegments,
+    world.plane.heightSegments
+  )
+
+  // vertice position randomization
+  const { array } = plane.geometry.attributes.position
+  const randomValues = []
+  for (let i = 0; i < array.length; i++) {
+    if (i % 3 === 0) {
+      const x = array[i]
+      const y = array[i + 1]
+      const z = array[i + 2]
+
+      //array[i] = //x + (Math.random() - 0.5) 
+      //array[i + 1] = //y + (Math.random() - 0.5) 
+      array[i + 2] = Math.sin(i)//z + (Math.random() - 0.5)
+    }
+
+    randomValues.push(Math.random() * Math.PI * 2)
+  }
+
+  plane.geometry.attributes.position.randomValues = randomValues;
+  plane.geometry.attributes.position.originalPosition = plane.geometry.attributes.position.array;
+
+  const colors = []
+  for (let i = 0; i < plane.geometry.attributes.position.count; i++) {
+    colors.push(0, 0.19, 0.4)
+  }
+
+  plane.geometry.setAttribute(
+    'color',
+    new THREE.BufferAttribute(new Float32Array(colors), 3)
+  )
+}
+
+function datGUI() {
+  const gui = new GUI();
+  gui.add(world.plane, 'width', 1, 200).onChange(generatePlane)
+  gui.add(world.plane, 'height', 1, 200).onChange(generatePlane)
+  gui.add(world.plane, 'widthSegments', 1, 100).onChange(generatePlane)
+  gui.add(world.plane, 'heightSegments', 1, 100).onChange(generatePlane) 
+  /*
+  var cam = gui.addFolder('camera')
+  cam.add(camera.position, 'z', 1, 500)  
+  cam.open()*/
+}
+
+
 // Initialize function
 function init() {
-  createCube();
+  //createCube();
   createAmbientLight();
-  //createPlane();
+  createPlane();
   window.addEventListener('resize', onWindowResize, false);
 }
 
@@ -80,85 +140,23 @@ function animate() {
   
 };
 
-
-
 // MAIN CODE
 // Scene, Camera, Renderer (SCR)
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-camera.position.set(0,1,10);
+camera.position.set(0,1,50);
 
-//
-//const renderer = new THREE.WebGLRenderer({
-//  canvas: document.querySelector('#bg'),
-//});
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+});
+//const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild(renderer.domElement)
-
-
+//document.body.appendChild(renderer.domElement)
+new OrbitControls(camera, renderer.domElement)
 
 init();
-animate();*/
+animate();
+datGUI();
 
-const scene = new THREE.Scene()
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-camera.position.z = 2
-
-const renderer = new THREE.WebGLRenderer()
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
-
-const controls = new OrbitControls(camera, renderer.domElement)
-//controls.addEventListener('change', render)
-
-const geometry = new THREE.BoxGeometry()
-const material = new THREE.MeshBasicMaterial({
-    color: 0x00ff00,
-    wireframe: true,
-})
-
-const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
-
-window.addEventListener('resize', onWindowResize, false)
-function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight
-    camera.updateProjectionMatrix()
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    render()
-}
-
-const stats = Stats()
-document.body.appendChild(stats.dom)
-
-const gui = new GUI()
-const cubeFolder = gui.addFolder('Cube')
-cubeFolder.add(cube.rotation, 'x', 0, Math.PI * 2)
-cubeFolder.add(cube.rotation, 'y', 0, Math.PI * 2)
-cubeFolder.add(cube.rotation, 'z', 0, Math.PI * 2)
-cubeFolder.open()
-const cameraFolder = gui.addFolder('Camera')
-cameraFolder.add(camera.position, 'z', 0, 10)
-cameraFolder.open()
-
-function animate() {
-    requestAnimationFrame(animate)
-
-    //stats.begin()
-    //cube.rotation.x += 0.01
-    //cube.rotation.y += 0.01
-    //stats.end()
-
-    render()
-
-    stats.update()
-}
-
-function render() {
-    renderer.render(scene, camera)
-}
-
-animate()

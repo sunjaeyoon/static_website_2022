f6a1666a335a6d17f9628a279d2a31f3cc0a2652
Tim <39254058+sunjaeyoon@users.noreply.github.com>
2022-01-10 00:34:24 -0500

    working globe

--- worldSphere/main.js
+++ worldSphere/main.js
@@ -5,6 +5,7 @@ https://www.youtube.com/watch?v=vM8M4QloVL0
 
 import './style.css';
 import * as THREE from 'three';
+import { GUI } from 'dat.gui'
 import glslify from 'glslify';
 import gsap from 'gsap';
 //import vertexShader from './shaders/vertexShader.glsl';
@@ -55,6 +56,7 @@ camera.position.set(0, 0, 10);
 
 const renderer = new THREE.WebGLRenderer({
     antialias: true,
+    canvas: document.querySelector('#bg')
 });
 renderer.setSize(innerWidth, innerHeight);
 
@@ -63,12 +65,6 @@ document.body.append(renderer.domElement)
   
 const sphere = new THREE.Mesh(
     new THREE.SphereGeometry(5, 50, 50),
-    /*
-    new THREE.MeshBasicMaterial({
-        //color: 0xff0000,
-        map: new THREE.TextureLoader().load('./images/globe.jpg')
-    })*/
-    
     new THREE.ShaderMaterial({
         vertexShader,
         fragmentShader, 
@@ -79,8 +75,15 @@ const sphere = new THREE.Mesh(
         }
     })
 );
-
+sphere.rotation.set(0.6, 0, 0);
 scene.add(sphere);
+
+function datGUI() {
+    const gui = new GUI();
+    gui.add(sphere.rotation, 'x', -3, 3).onChange()
+    gui.add(sphere.rotation, 'y', -3, 3).onChange()
+    gui.add(sphere.rotation, 'z', -3, 3).onChange()
+}
 
 const atmosphere = new THREE.Mesh(
     new THREE.SphereGeometry(5, 50, 50),
@@ -99,6 +102,37 @@ const group = new THREE.Group();
 group.add(sphere)
 scene.add(group)
 
+const starGeometry = new THREE.BufferGeometry();
+const vertices = [];
+const dv = [];
+
+for ( let i = 0; i < 10000; i ++ ) {
+
+    const x = 2000 * Math.random() - 1000;
+    const y = 2000 * Math.random() - 1000;
+    const z = 10*Math.random() - 100;
+
+    const dx = Math.random();
+    const dy = Math.random();
+    const dz = 0;
+
+    vertices.push( x, y, z );
+    dv.push( dx, dy , dz);
+}
+
+starGeometry.setAttribute( 'position', new THREE.Float32BufferAttribute( vertices, 3 ) );
+starGeometry.setAttribute( 'dposition', new THREE.Float32BufferAttribute( dv, 3 ));
+
+const stars = new THREE.Points(
+    starGeometry,
+    new THREE.PointsMaterial({
+        size: 0.5
+    })
+)
+
+console.log(starGeometry)
+scene.add(stars)
+
 const mouse = {
     x: 0,
     y: 0,
@@ -108,14 +142,24 @@ function animate() {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
     sphere.rotation.y += 0.002;
-    group.rotation.y = mouse.x * 0.5;
+
     gsap.to(group.rotation, {
-        y: -mouse.y * 0.5,
-        x: mouse.x * 0.5
+        y: -mouse.y * 0.2,
+        x: mouse.x * 0.2
     })
+
 }
 addEventListener('mousemove', (e)=>{
     mouse.x = (e.clientX/innerWidth) * 2 - 1;
     mouse.y = (e.clientY/innerHeight) * 2 + 1;
 })
+addEventListener('resize', onWindowResize)
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+
+
 animate();
+datGUI();

b4e4e6faf0d5927c4b0c7927c4640293bd919811
Tim <39254058+sunjaeyoon@users.noreply.github.com>
2022-03-06 16:23:37 -0500

    template for future use

---
+++ template/src/shaders/fragment.glsl
@@ -0,0 +1,5 @@
+varying vec2 vCoordinates;
+
+void main(){
+    gl_FragColor = vec4(vCoordinates.x/512. , vCoordinates.y/512. , 0, 1.0);
+}
--- template/src/shaders/vertex.glsl
+++ template/src/shaders/vertex.glsl
@@ -7,7 +7,7 @@ void main(){
 
     vec4 myPosition = modelViewMatrix * vec4(position, 1.0);
     
-    gl_PointSize = 500. * (1./ -myPosition.z);
+    gl_PointSize = 400. * (1./ -myPosition.z);
     gl_Position = projectionMatrix * myPosition;
 
     vCoordinates = aCoordinates.xy;

fda3f4b42d6f1b29411bf54e0351316cbe51872c
Tim <39254058+sunjaeyoon@users.noreply.github.com>
2022-03-19 13:54:35 -0400

    notes

--- template/src/shaders/fragment.glsl
+++ template/src/shaders/fragment.glsl
@@ -1,5 +1,6 @@
 varying vec2 vCoordinates;
 
 void main(){
-    gl_FragColor = vec4(vCoordinates.x/512. , vCoordinates.y/512. , 0, 1.0);
+    gl_FragColor = vec4(vCoordinates.x/512. , vCoordinates.y/512. , 0, 1.0); //Custom Geometry
+    //gl_FragColor = vec4(vUv, 0., 1.); //THREEjs Geometry 
 }
--- template/src/shaders/vertex.glsl
+++ template/src/shaders/vertex.glsl
@@ -3,12 +3,13 @@ varying vec2 vCoordinates;
 attribute vec3 aCoordinates;
 
 void main(){
-    vUv = uv;
+    
 
     vec4 myPosition = modelViewMatrix * vec4(position, 1.0);
     
     gl_PointSize = 400. * (1./ -myPosition.z);
     gl_Position = projectionMatrix * myPosition;
 
-    vCoordinates = aCoordinates.xy;
+    vCoordinates = aCoordinates.xy; //Custom Geometry
+    vUv = uv; // THREEjs normals
 } 
